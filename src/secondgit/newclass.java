e0addfc77f36470148030c0a63dea745c885e4ab
yogalakshmi08 <syogalakshmi08@gmail.com>
2018-10-25 06:04:14 -0700

    added a method

--- src/secondgit/newclass.java
+++ src/secondgit/newclass.java
@@ -1,7 +1,12 @@
 package secondgit;
 
 public class newclass {
+	public void work()
+	{
+		System.out.println("Working");
+	}
 public static void main(String[] args) {
-	System.out.println("Working");
+	newclass ob=new newclass();
+	ob.work();
 }
 }
